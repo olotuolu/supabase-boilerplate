4ebaeecc7cca6806e6f604b5b8e7a4681171ea80
Oluwanifemi Olotu <olotuolu@msu.edu>
2023-03-19 19:35:07 -0400

    Java tweek

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Add JavaScript below
-import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.10.0"
+
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ikgbjhytedihspzbauid.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrZ2JqaHl0ZWRpaHNwemJhdWlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1MzEyODYsImV4cCI6MTk5MzEwNzI4Nn0.MCAVdo9IL7Kda8sJ8A40pwZ_PHvwD-7G9lA2fARuC80'
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrZ2JqaHl0ZWRpaHNwemJhdWlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1MzEyODYsImV4cCI6MTk5MzEwNzI4Nn0.MCAVdo9IL7Kda8sJ8A40pwZ_PHvwD-7G9lA2fARuC80
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function getBooks(){
